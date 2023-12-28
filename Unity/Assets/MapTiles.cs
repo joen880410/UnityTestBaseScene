f403d9d08f516c3092cc07a8390c85ee0994729a
joen880410 <joen0154@gmail.com>
2023-12-28 20:56:34 +0800

    [+]新增地圖方塊

---
+++ Unity/Assets/MapTiles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class MapTiles : ScriptableObject
+{
+    public int price;
+    public int mapType;
+    public Sprite mapImg;
+    public bool isCanBuy;
+    public bool isCanUpgrade;
+
+    [Tooltip("轉動玩家角度，控制玩家前進方向")]
+    public Vector3 playerRotationAngle;
+
+    public long owneruid;
+    [Header("地圖目前狀態")]
+    public long stage;
+}
